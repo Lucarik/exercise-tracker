6f507b3bd6efb146605244ad759ca4b2c8897bca
Justin Sterling <justinsterling44@gmail.com>
2023-11-25 23:06:23 -0500

    Added more get methods

--- index.js
+++ index.js
@@ -27,6 +27,17 @@ const createAndSavePerson = (n, done) => {
   });
 };
 
+const getAllUsers = (done) => {
+  Person.find({}, function(err, users) {
+    if (err) return err;
+    var userArray = [];
+    users.forEach(function(user) {
+      userArray.push({_id:user._id, username:user.username});
+    });
+    done(null, userArray);  
+  });
+}
+
 const findPersonById = (personId, done) => {
   Person.findById(personId, function(err, data) {
     if (err) return err;
@@ -37,7 +48,7 @@ const findPersonById = (personId, done) => {
 const findEditThenSave = (personId, description, duration, date, done) => {
   const foodToAdd = "hamburger";
   Person.findById(personId, function(err, person) {
-    person.exercises.push({description: description, duration: duration, date: date})
+    person.exercises.push({description: description, duration: +duration, date: (new Date(date)).toString().substring(0,15)})
     person.save(function(err, data) {
       if (err) return err;
       done(null, data);
@@ -56,15 +67,22 @@ app.use(bodyParser.urlencoded({extended: false}));
 app.post("/api/users", function(req,res) {
     createAndSavePerson(req.body.username, function(error, person) {
       if (error) return next(error);
-      res.json({id: person._id, username: person.username});
+      res.json({_id: person._id, username: person.username});
     });
+});
+
+app.get("/api/users", function(req,res) {
+  getAllUsers(function(error, users) {
+    if (error) return next(error);
+    res.json(users);
+  });
 });
 
 app.post("/api/users/:_id/exercises", function(req,res) {
   findEditThenSave(req.params._id, req.body.description, req.body.duration, req.body.date, function(error, person) {
     if (error) return next(error);
     let exercise = person.exercises[person.exercises.length-1]
-    res.json({id: person._id, username: person.username, 
+    res.json({_id: person._id, username: person.username, 
       description: exercise.description,
       duration: exercise.duration,
       date: exercise.date,});
@@ -76,40 +94,41 @@ app.get("/api/users/:_id/logs?*", function(req, res) {
   for (let p of Object.keys(req.query)) {
     params.push(p.replace(/[\[\]]/g, ""));
   }
-  //res.json(params);
-  console.log(req.params._id);
+  let dateParams = Object.keys(req.query).length != 0;
   let id = req.params._id;
-  let from = params[0];
-  let to = params[1];
-  let limit = params[2];
+  if (dateParams) {
+    
+    var from = params[0];
+    var to = params[1];
+    var limit = params[2];
+  }
 
   findPersonById(id, function(error, person) {
     if (error) return next(error);
       let exercises = person.exercises;
       let matched = [];
       let c = 0;
-      if (typeof exercises === undefined) res.json({error: "This user has no saved exercises"});
-      for (let exercise of exercises) {
-        if (c >= limit) break;
-        //console.log("from: " + from + ", to: " + to + "\n" + exercise)
-        if (exercise.date >= from && exercise.date <= to) {
-          matched.push(exercise);
-          c += 1;
+      
+      if (typeof exercises === undefined) res.json({id: person._id, username: person.username,
+        count: count, log: "This user has no saved exercises"});
+      if (dateParams) {
+        for (let exercise of exercises) {
+          console.log(exercise.date);
+          if (c >= limit) break;
+          //console.log("from: " + from + ", to: " + to + "\n" + exercise)
+          if ((new Date(exercise.date)).toISOString().substring(0,10) >= from 
+          && (new Date(exercise.date)).toISOString().substring(0,10) <= to) {
+            matched.push(exercise);
+            c += 1;
+          }
         }
+      } else {
+        matched = exercises;
       }
-      if (matched.length == 0) res.json({error: "No exercises matched"});
-      res.json(matched);
-  });
-});
-
-app.get("/api/users/:_id/logs", function(req, res) {
-  findPersonById(req.params._id, function(error, person) {
-    if (error) return next(error);
-      let exercises = person.exercises;
-      if (typeof exercises === undefined) res.json({error: "This user has no saved exercises"});
       let count = exercises.length;
+      if (matched.length == 0) res.json({error: "No exercises matched"});
       res.json({id: person._id, username: person.username,
-      count: count, exercises: exercises});
+        count: count, log: matched});
   });
 });
 
